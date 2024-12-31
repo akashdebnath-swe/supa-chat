68676d1b6e72a2eb1848765a586e900fc853a396
theaakashd <akashdebnathwd@gmail.com>
2024-12-31 08:14:14 +0530

    fixing deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatAbout from "@/components/chat-about";
 import ChatContainer from "@/components/chat-container";
 import ChatHeader from "@/components/chat-header";
 import ChatInput from "@/components/chat-input";
-import InitUser from "@/lib/store/InitUser";
+import InitUser from "../lib/store/initUser";
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 
 export default async function Home() {
@@ -13,8 +13,8 @@ export default async function Home() {
     } = await supabase.auth.getUser();
 
     return (
-        <main className='w-full md:max-w-md mx-auto md:py-10 h-screen'>
-            <div className='h-full border rounded-md flex flex-col relative'>
+        <main className="w-full md:max-w-md mx-auto md:py-10 h-screen">
+            <div className="h-full border rounded-md flex flex-col relative">
                 <ChatHeader user={user} />
                 {user ? (
                     <>
